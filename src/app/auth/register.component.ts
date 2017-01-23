2d201f47918846b720ca56c69e5bff1f94657d35
Rahul Kumar <rahul.x@venturepact.com>
2017-01-23 09:51:53 +0530

    Registration component finalised!

--- src/app/auth/register.component.ts
+++ src/app/auth/register.component.ts
@@ -1,21 +1,36 @@
-import {Component, OnInit} from "@angular/core";
-import { User } from "../shared/models/user";
-import { Address } from "../shared/models/address";
-
+import { Component } from '@angular/core';
+import { User } from './user';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
-    selector: 'app-register',
-    templateUrl: './register.component.html',
-    styles: []
+  selector: 'app-register',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class SignupFormComponent {
+	signupForm : FormGroup;
 
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
-
+	constructor(fb: FormBuilder){
+    let password = new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)]));
+    let confirmPassword = new FormControl('', CustomValidators.equalTo(password));
 
 
+		this.signupForm = fb.group({
+			'name' : [null, Validators.required],
+			'email' : [null, Validators.required],
+			'password': password,
+      'confirmPassword': confirmPassword,
+      'acceptPolicy' : [false, CustomValidators.equal(true)]
+		});
+	}
+
+  user = new User;
+
+  onSubmit(value : any){
+  	this.user.name = value.name;
+  	this.user.email = value.email;
+  	this.user.password = value.password;
+  }
+  
 }
