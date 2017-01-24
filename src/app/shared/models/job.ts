4eeffede1006b456439bf4ff65a67c24ec0a7c63
Rahul Kumar <rahul.x@venturepact.com>
2017-01-24 12:05:39 +0530

    Profile editing Component finalised, New Model Job added!

---
+++ src/app/shared/models/job.ts
@@ -0,0 +1,13 @@
+export class Job {
+    job_id: string;
+    category: string;
+    title: string;
+    description: string;
+    ageLimit: string;
+    workExp: string;
+    eduDetails: string;
+    lastDate: Date;
+    joiningDate: Date;
+    workPlace: string;
+    postedBy: string;
+}
