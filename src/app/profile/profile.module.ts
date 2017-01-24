ecfdbdbab0bb5771d1bbb6a2cd014a0c0127c6c3
rahul kumar <rahulkumar2048@gmail.com>
2017-01-24 12:35:37 +0530

    fixed several bugs

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,36 +1,29 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProfileComponent} from "./profile.component";
-import { RouterModule, Routes } from '@angular/router';
-import { UserdetailsComponent } from './userdetails.component';
+import {RouterModule, Routes} from '@angular/router';
+import {UserdetailsComponent} from './userdetails.component';
 import {EditComponent} from './edit.component';
-import { ImageUploadModule } from 'ng2-imageupload';
 
 
-const APP_ROUTES:Routes=[
-    {path:'profile',component:ProfileComponent,
-    children:[
-        {
-            path:'',
-            component:UserdetailsComponent
-        },
-        {
-            path:'edit',
-            component:EditComponent
-        }
-    ]
-   
-
-}
+const APP_ROUTES: Routes = [
+    {
+        path: 'profile', component: ProfileComponent,
+        children: [
+            {
+                path: '',
+                component: UserdetailsComponent
+            }
+        ]
+    }
 ];
 
 @NgModule({
     imports: [
         CommonModule,
-         RouterModule.forRoot(APP_ROUTES),
-         ImageUploadModule 
+        RouterModule.forRoot(APP_ROUTES),
     ],
-    declarations: [ProfileComponent, UserdetailsComponent,EditComponent]
+    declarations: [ProfileComponent, UserdetailsComponent, EditComponent]
 })
 export class ProfileModule {
 }
